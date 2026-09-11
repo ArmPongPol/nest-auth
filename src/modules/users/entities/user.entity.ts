1717a28660816d821926a066bcabb8c33b2732f3
Pongpol Prommacharoen/Arm <MTL92354@murata.com>
2026-09-11 17:05:31 +0700

    first commit

---
+++ src/modules/users/entities/user.entity.ts
@@ -0,0 +1,51 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'varchar',
+    length: 150,
+    unique: true,
+  })
+  email: string;
+
+  @Column({
+    type: 'varchar',
+  })
+  password: string;
+
+  @Column({
+    type: 'varchar',
+    unique: true,
+    name: 'display_name',
+  })
+  displayName: string;
+
+  @Column({
+    name: 'is_active',
+    default: true,
+    type: 'bool',
+  })
+  isActive: boolean;
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+    name: 'created_at',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    name: 'updated_at',
+  })
+  updatedAt: Date;
+}
